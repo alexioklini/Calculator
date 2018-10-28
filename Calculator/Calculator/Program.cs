e29365020b8cf05765f58b708c1408342c2aa986
Alexander Klinsky <alexander.klinsky@me.com>
2018-10-28 19:10:37 +0100

    copyright

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -6,7 +6,7 @@ namespace Brilliant
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Brilliant 1.0");
+            Console.WriteLine("Brilliant 1.0 Copyright (c) 2018 Alex Klinsky");
         }
     }
 }
